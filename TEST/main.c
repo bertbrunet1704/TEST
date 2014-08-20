c2df322dd2130b02a767db41403c3154ac6c7176
Bertrand Brunet <bertrandbrunet@bertrandbrunet.home>
2014-08-20 21:48:20 +0400

    Initial Commit

---
+++ TEST/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  TEST
+//
+//  Created by Bertrand Brunet on 8/20/14.
+//  Copyright (c) 2014 Bertrand. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
